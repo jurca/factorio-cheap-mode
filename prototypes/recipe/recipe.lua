d54595bd6474af54e3aa5c69229476f14eb9ab41
Martin Jurča <martin.jurca@firma.seznam.cz>
2018-08-14 20:22:42 +0200

    First public release

---
+++ prototypes/recipe/recipe.lua
@@ -0,0 +1,42 @@
+recipes = data.raw.recipe
+
+-- https://github.com/wube/factorio-data/blob/master/base/prototypes/recipe/ammo.lua
+recipes['cannon-shell'].normal.ingredients[1][2] = 1
+recipes['cannon-shell'].normal.ingredients[2][2] = 1
+recipes['explosive-cannon-shell'].normal.ingredients[1][2] = 1
+recipes['explosive-cannon-shell'].normal.ingredients[2][2] = 1
+
+-- https://github.com/wube/factorio-data/blob/master/base/prototypes/recipe/demo-recipe.lua
+recipes['steam-engine'].normal.ingredients[1][2] = 6
+recipes['steam-engine'].normal.ingredients[3][2] = 2
+recipes['iron-gear-wheel'].normal.ingredients[1][2] = 1
+recipes['electronic-circuit'].normal.result_count = 2
+recipes['electric-mining-drill'].normal.ingredients[1][2] = 1
+recipes['electric-mining-drill'].normal.ingredients[2][2] = 3
+recipes['electric-mining-drill'].normal.ingredients[3][2] = 5
+recipes['burner-mining-drill'].normal.ingredients[1][2] = 2
+recipes['burner-mining-drill'].normal.ingredients[3][2] = 2
+recipes['pipe'].normal.result_count = 2
+recipes['submachine-gun'].normal.ingredients[1][2] = 5
+recipes['submachine-gun'].normal.ingredients[2][2] = 1
+recipes['submachine-gun'].normal.ingredients[3][2] = 3
+
+-- https://github.com/wube/factorio-data/blob/master/base/prototypes/recipe/furnace-recipe.lua
+recipes['steel-plate'].normal.result_count = 2
+
+-- https://github.com/wube/factorio-data/blob/master/base/prototypes/recipe/recipe.lua
+recipes['express-transport-belt'].normal.ingredients[1][2] = 5
+recipes['assembling-machine-2'].normal.ingredients[1][2] = 1
+recipes['assembling-machine-2'].normal.ingredients[2][2] = 3
+recipes['assembling-machine-2'].normal.ingredients[3][2] = 3
+recipes['tank'].normal.ingredients[1][2] = 16
+recipes['tank'].normal.ingredients[2][2] = 25
+recipes['tank'].normal.ingredients[3][2] = 8
+recipes['tank'].normal.ingredients[4][2] = 5
+recipes['advanced-circuit'].normal.ingredients[2][2] = 1
+recipes['advanced-circuit'].normal.ingredients[3][2] = 2
+recipes['processing-unit'].normal.ingredients[3].amount = 3
+recipes['explosives'].normal.result_count = 4
+recipes['battery'].normal.ingredients[1].amount = 10
+recipes['low-density-structure'].normal.ingredients[2][2] = 3
+recipes['low-density-structure'].normal.ingredients[3][2] = 3
