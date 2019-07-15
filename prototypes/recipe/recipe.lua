8f835d287ac91791e3f1fb47764bea72f53a8324
Martin Jurča <mjurca@centrum.cz>
2019-07-15 21:53:59 +0200

    updated recipes for factorio 0.17

--- prototypes/recipe/recipe.lua
+++ prototypes/recipe/recipe.lua
@@ -1,10 +1,15 @@
 recipes = data.raw.recipe
-
+-- expensive
 -- https://github.com/wube/factorio-data/blob/master/base/prototypes/recipe/ammo.lua
 recipes['cannon-shell'].normal.ingredients[1][2] = 1
 recipes['cannon-shell'].normal.ingredients[2][2] = 1
 recipes['explosive-cannon-shell'].normal.ingredients[1][2] = 1
 recipes['explosive-cannon-shell'].normal.ingredients[2][2] = 1
+
+-- https://github.com/wube/factorio-data/blob/master/base/prototypes/recipe/capsule.lua
+
+-- https://github.com/wube/factorio-data/blob/master/base/prototypes/recipe/demo-furnace-recipe.lua
+recipes['steel-plate'].normal.ingredients[1][2] = 3
 
 -- https://github.com/wube/factorio-data/blob/master/base/prototypes/recipe/demo-recipe.lua
 recipes['steam-engine'].normal.ingredients[1][2] = 6
@@ -20,15 +25,25 @@ recipes['pipe'].normal.result_count = 2
 recipes['submachine-gun'].normal.ingredients[1][2] = 5
 recipes['submachine-gun'].normal.ingredients[2][2] = 1
 recipes['submachine-gun'].normal.ingredients[3][2] = 3
+recipes['assembling-machine-2'].normal.ingredients[1][2] = 1
+recipes['assembling-machine-2'].normal.ingredients[2][2] = 1
+recipes['assembling-machine-2'].normal.ingredients[3][2] = 3
+
+-- https://github.com/wube/factorio-data/blob/master/base/prototypes/recipe/demo-turret.lua
+
+-- https://github.com/wube/factorio-data/blob/master/base/prototypes/recipe/equipment.lua
+
+-- https://github.com/wube/factorio-data/blob/master/base/prototypes/recipe/fluid-recipe.lua
 
 -- https://github.com/wube/factorio-data/blob/master/base/prototypes/recipe/furnace-recipe.lua
-recipes['steel-plate'].normal.result_count = 2
+recipes['steel-plate'].normal.result_count = 3
+
+-- https://github.com/wube/factorio-data/blob/master/base/prototypes/recipe/inserter.lua
+
+-- https://github.com/wube/factorio-data/blob/master/base/prototypes/recipe/module.lua
 
 -- https://github.com/wube/factorio-data/blob/master/base/prototypes/recipe/recipe.lua
 recipes['express-transport-belt'].normal.ingredients[1][2] = 5
-recipes['assembling-machine-2'].normal.ingredients[1][2] = 1
-recipes['assembling-machine-2'].normal.ingredients[2][2] = 3
-recipes['assembling-machine-2'].normal.ingredients[3][2] = 3
 recipes['tank'].normal.ingredients[1][2] = 16
 recipes['tank'].normal.ingredients[2][2] = 25
 recipes['tank'].normal.ingredients[3][2] = 8
@@ -38,5 +53,6 @@ recipes['advanced-circuit'].normal.ingredients[3][2] = 2
 recipes['processing-unit'].normal.ingredients[3].amount = 3
 recipes['explosives'].normal.result_count = 4
 recipes['battery'].normal.ingredients[1].amount = 10
-recipes['low-density-structure'].normal.ingredients[2][2] = 3
-recipes['low-density-structure'].normal.ingredients[3][2] = 3
+recipes['low-density-structure'].normal.ingredients[3][2] = 1
+
+-- https://github.com/wube/factorio-data/blob/master/base/prototypes/recipe/turret.lua
